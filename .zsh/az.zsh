878eb7143b4d2a9a128d5eff7cd020e70a4d3235
Fernando de Morais <mnando@gmail.com>
2025-06-19 10:59:02 -0700

    Fix azure-cli config

--- .zsh/az.zsh
+++ .zsh/az.zsh
@@ -3,7 +3,7 @@ if [ -f /etc/bash_completion.d/azure-cli ]; then
 else
     [[ -f "${HOME}/lib/azure-cli/az.completion" ]] && source ${HOME}/lib/azure-cli/az.completion
 
-    if [ -f "$(brew --prefix)/etc/bash_completion.d/az" ]; then
+    if command -v brew >/dev/null 2>&1 && [ -f "$(brew --prefix)/etc/bash_completion.d/az" ]; then
         autoload bashcompinit && bashcompinit
         source "$(brew --prefix)/etc/bash_completion.d/az"
     fi
